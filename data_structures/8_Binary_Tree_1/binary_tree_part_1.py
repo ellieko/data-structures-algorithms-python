c05e9c2e0b375c7d726831e7ebb9cb1a6eb0b057
Ellie Ko <70093034+ellieko@users.noreply.github.com>
2022-01-03 16:27:45 -0600

    answers for binary tree (part 1) practice probs

--- data_structures/8_Binary_Tree_1/binary_tree_part_1.py
+++ data_structures/8_Binary_Tree_1/binary_tree_part_1.py
@@ -47,6 +47,54 @@ class BinarySearchTreeNode:
 
         return elements
 
+    # practice problems - implement five methods below
+
+    # finds minimum element in entire binary tree
+    def find_min(self):
+        if self.left:
+            return self.left.find_min()
+        else:
+            return self.data
+
+    # finds maximum element in entire binary tree
+
+    def find_max(self):
+        if self.right:
+            return self.right.find_max()
+        else:
+            return self.data
+
+    # calcualtes sum of all elements
+    def calculate_sum(self):
+        sum = self.data
+        if self.left:
+            sum += self.left.calculate_sum()
+        if self.right:
+            sum += self.right.calculate_sum()
+        return sum
+
+    # perofrms pre order traversal of a binary tree
+    def pre_order_traversal(self):
+        elements = []
+        elements.append(self.data)
+
+        if self.left:
+            elements += self.left.pre_order_traversal()
+        if self.right:
+            elements += self.right.pre_order_traversal()
+        return elements
+
+    # performs post order traversal of a binary tree
+    def post_order_traversal(self):
+        elements = []
+        if self.left:
+            elements += self.left.post_order_traversal()
+        if self.right:
+            elements += self.right.post_order_traversal()
+
+        elements.append(self.data)
+        return elements
+
 
 def build_tree(elements):
     print("Building tree with these elements:", elements)
@@ -56,17 +104,6 @@ def build_tree(elements):
         root.add_child(elements[i])
 
     return root
-
-
-'''
-Add following methods to [BinarySearchTreeNode class](https://github.com/codebasics/data-structures-algorithms-python/blob/master/data_structures/8_Binary_Tree_1/binary_tree_part_1.py) created in main video tutorial
-
-    1. find_min(): finds minimum element in entire binary tree
-    2. find_max(): finds maximum element in entire binary tree
-    3. calculate_sum(): calcualtes sum of all elements
-    4. post_order_traversal(): performs post order traversal of a binary tree
-    5. pre_order_traversal(): perofrms pre order traversal of a binary tree
-'''
 
 
 if __name__ == '__main__':
@@ -80,3 +117,32 @@ if __name__ == '__main__':
     numbers_tree = build_tree([17, 4, 1, 20, 9, 23, 18, 34])
     print("In order traversal gives this sorted list:",
           numbers_tree.in_order_traversal())
+
+    print("\n- - - - - - - - - - - - - - - - -\n")
+
+    print(numbers_tree.find_min())
+    print(numbers_tree.find_max())
+
+    print(sum([17, 4, 1, 20, 9, 23, 18, 34]))
+    print(numbers_tree.calculate_sum())
+
+    print("Pre order traversal gives this sorted list:",
+          numbers_tree.pre_order_traversal())
+
+    print("Post order traversal gives this sorted list:",
+          numbers_tree.post_order_traversal())
+
+    print("\n- - - - - - - - - - - - - - - - -\n")
+
+    numbers = [17, 4, 1, 20, 9, 23, 18, 34]
+
+    numbers = [15, 12, 7, 14, 27, 20, 23, 88]
+
+    numbers_tree = build_tree(numbers)
+    print("Input numbers:", numbers)
+    print("Min:", numbers_tree.find_min())
+    print("Max:", numbers_tree.find_max())
+    print("Sum:", numbers_tree.calculate_sum())
+    print("In order traversal:", numbers_tree.in_order_traversal())
+    print("Pre order traversal:", numbers_tree.pre_order_traversal())
+    print("Post order traversal:", numbers_tree.post_order_traversal())
