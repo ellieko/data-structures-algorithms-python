17d2de163ba65c9eda5a67ca0ddc872bca6e44a6
Ellie Ko <70093034+ellieko@users.noreply.github.com>
2022-01-07 20:12:40 -0600

    answers for bubble sort practice probs

--- algorithms/2_BubbleSort/bubble_sort.py
+++ algorithms/2_BubbleSort/bubble_sort.py
@@ -17,7 +17,32 @@ def bubble_sort(elements):
 
         if not swapped:
             break
+'''
+exercise prob)
+modify bubble sort s.t. it can sort following list of transactions with key
+elements = [
+        { 'name': 'mona',   'transaction_amount': 1000, 'device': 'iphone-10'},
+        { 'name': 'dhaval', 'transaction_amount': 400,  'device': 'google pixel'},
+        { 'name': 'kathy',  'transaction_amount': 200,  'device': 'vivo'},
+        { 'name': 'aamir',  'transaction_amount': 800,  'device': 'iphone-8'},
+    ]
+'''
 
+# elements: list of hash tables
+def bubble_sort_exercise(elements, key):
+    size = len(elements)
+
+    for i in range(size-1):
+        swapped = False
+        for j in range(size-1-i):
+            if elements[j][key] > elements[j+1][key]:
+                tmp = elements[j]
+                elements[j] = elements[j+1]
+                elements[j+1] = tmp
+                swapped = True
+
+        if not swapped:
+            break
 
 if __name__ == '__main__':
     elements = [5,9,2,1,67,34,88,34]
@@ -26,3 +51,17 @@ if __name__ == '__main__':
 
     bubble_sort(elements)
     print(elements)
+
+    print("\n- - - - - - - - - - - - - - - - -\n")
+
+    elements = [
+        { 'name': 'mona',   'transaction_amount': 1000, 'device': 'iphone-10'},
+        { 'name': 'dhaval', 'transaction_amount': 400,  'device': 'google pixel'},
+        { 'name': 'kathy',  'transaction_amount': 200,  'device': 'vivo'},
+        { 'name': 'aamir',  'transaction_amount': 800,  'device': 'iphone-8'},
+    ]   
+    
+    bubble_sort_exercise(elements, 'name')
+    print(f"sorted by name:\n{elements}\n") 
+    bubble_sort_exercise(elements, 'transaction_amount')
+    print(f"sorted by transaction amount:\n{elements}\n") 
